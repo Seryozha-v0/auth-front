9b4228579278245fa0f92bd90e096f6d205454d9
Seryozha <arzumanyan.seroj@mail.ru>
2022-12-11 20:08:57 +0300

    test URL for vercel

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_BACK_URL,
+    // baseURL: process.env.REACT_BACK_URL,
+    baseURL: 'https://auth-back-six.vercel.app',
     withCredentials: true
 });
 
