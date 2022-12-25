245f2f1ba4da223069f81ffc44c4cd1bd12db7e9
Seryozha <arzumanyan.seroj@mail.ru>
2022-12-25 23:06:17 +0300

    axios baseUrl

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: process.env.REACT_BACK_URL,
-    // baseURL: 'https://auth-back-six.vercel.app',
-    baseURL: 'http://localhost:4400',
+    baseURL: 'https://auth-back-six.vercel.app',
+    // baseURL: 'http://localhost:4400',
     withCredentials: true
 });
 
